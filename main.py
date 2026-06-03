317578db2727c004015317742d5de77cf82d7904
Даниил <sergeevdaniil383@gmail.com>
2026-06-03 13:04:22 +0300

    Добавлен прокси, токен уехал в env

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import asyncio
 from os import getenv
+from dotenv import load_dotenv
 
 from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.client.session.aiohttp import AiohttpSession
 
-TOKEN = "8894028446:AAEpT-Tl6NPNGZGvbsYCTPDiHcFeWO1sJwE"
+load_dotenv()
+
+TOKEN = getenv("TOKEN")
+PROXY_URL = getenv("PROXY_URL")
 
 dp = Dispatcher()
+
 
 @dp.message(Command("start"))
 async def command_start_handler(message: Message) -> None:
     await message.answer("Hello! I'm a bot created with aiogram.")
 
 
-#ЗАПУСК БОТА
+# ЗАПУСК БОТА
 async def main() -> None:
-    bot = Bot(token=TOKEN)
-    print("Бот запущен!")
+    session = AiohttpSession(proxy=PROXY_URL)
+    bot = Bot(token=TOKEN, session=session)
+    print("Бот запущен через прокси!")
     await dp.start_polling(bot)
 
 
