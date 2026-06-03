1ea07b80f47f3741f69025984ba8b8b2f19b733d
Даниил <sergeevdaniil383@gmail.com>
2026-06-04 00:19:41 +0300

    проверка7

--- data/db_manager.py
+++ data/db_manager.py
@@ -1,3 +1,5 @@
+from sqlalchemy import false
+
 from . import db_session
 from .users import User
 
@@ -15,3 +17,30 @@ def new_or_old_user_check_and_create(telegram_id):
         )
     db_sess.close()
     return user
+
+
+def check_user_is_allowed(telegram_id):
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.telegram_id == telegram_id).first()
+
+    if not user:
+        db_sess.close()
+        return False
+    is_allowed = user.is_allowed
+    db_sess.close()
+
+    return is_allowed
+
+
+
+def chech_user_is_admin(telegram_id):
+    db_sess = db_session.create_session()
+    user = db_sess.query(User).filter(User.telegram_id == telegram_id).first()
+
+    if not user:
+        db_sess.close()
+        return False
+    is_admin = user.is_admin
+    db_sess.close()
+
+    return is_admin
